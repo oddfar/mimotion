1e830a8cdf1a5d36eeb3b3a6769260733c67cdc5
ocgoodoc <69777345+ocgoodoc@users.noreply.github.com>
2023-12-02 22:44:42 +0800

    Update githubRun.py

--- githubRun.py
+++ githubRun.py
@@ -195,6 +195,6 @@ if __name__ == '__main__':
     password = sys.argv[2]
     pushplus_token = sys.argv[3]
     ##刷步数
-    res = main(user, password, 5000, 8000)
+    res = main(user, password, 15000, 18000)
     # 推送消息
     pushMessage(pushplus_token, "刷步接口调用", res)
